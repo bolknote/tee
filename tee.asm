53694892dc1c535e89ebf0f69d5fc4f9c337a88d
Evgeny Stepanischev <Evgeny.Stepanischev@tatar.ru>
2018-01-31 23:45:37 +0300

    Added support for several filenames and append flag

--- tee.asm
+++ tee.asm
@@ -1,3 +1,5 @@
+; nasm -f elf64 tee.asm && ld -s -o tee tee.o
+
 SYS_EXIT	equ 60
 SYS_READ	equ 0
 SYS_WRITE	equ 1
@@ -10,68 +12,84 @@ STDOUT		equ 1
 STDERR		equ 2
 
 O_WRONLY 	equ 0x001
+O_CREAT		equ 0x040
+O_TRUNC		equ 0x200
+O_APPEND	equ 0x400
 
 CHUNK		equ 8192
+MAXPARAMS	equ 100
 
 section .data
-	errorStr db `Error opening file for writing.\n`
-	errorLen equ $ - errorStr
+	errorStr db `: Error opening file for writing\n`
+	.len equ $ - errorStr
 
 section .bss
 	buffer resb CHUNK
+	handlers resq MAXPARAMS
 
 section .text
 	global _start
 
 _start:
-	xor r15, r15 ; second file descriptor or 0 if none
+	xor r15, r15 ; append flag
+	mov r14, handlers ; file handlers table
 
-	pop rax ; argc
-	cmp rax, 1
-	jle loop
+	mov rax, STDOUT
+	call store_filehandler
 
-	pop rsi ; *argv[0]
-	pop rsi ; *argv[1]
+	pop rcx ; argc
+	cmp rcx, 1
+	jle tee
 
-	cmp word [rsi], `-\0`
-	je alsostdout
-
-filename:
 	cld
-	mov rcx, CHUNK
-	xor rax, rax
-	mov rdi, rsi
-	repne scasb ; rdi - rsi = length
+	pop rsi ; skip *argv[0]
+	dec rcx
 
-	sub rdi, rsi
-	mov rcx, rdi
-	mov rdi, buffer
-	rep movsb ; copy argv[1] to buffer
+	cmp rcx, MAXPARAMS
+	jle next_arg
 
-	mov rax, SYS_CREAT
-	mov rdi, buffer
-	mov rsi, 0o660
-	syscall
+	mov rax, MAXPARAMS
+
+next_arg:
+	pop rsi ; *argv[]
+	push rcx
+
+	mov eax, dword [rsi]
+
+	and eax, 0x00FFFFFF ; clear last byte
+	cmp eax, '-a'		; -a\0
+	je append_flag
+	cmp ax, `-\0`		; -\0?
+	je file_stdout
+
+	;real filename
+	call copy2buf ; rdx — length
+
+	push rdx
+	call open_file ; rax — file handler, 0 if error
+	pop rdx
 
 	cmp rax, 0
-	jle error
+	jg file_real
 
-	mov r15, rax
-	jmp loop
+	call file_error
+	jmp arg_loop
 
-alsostdout:
-	; second descriptor is also STDOUT
-	mov r15, STDOUT
-	jmp loop
+file_stdout:
+	mov rax, STDOUT
+file_real:
+	call store_filehandler
+	jmp arg_loop
 
-error:
-	mov rax, SYS_WRITE
-	mov rdi, STDERR
-	mov rsi, errorStr
-	mov rdx, errorLen
-	syscall
+append_flag:
+	; set append flag
+	inc r15
 
-loop:
+arg_loop:
+	pop rcx
+	loopnz next_arg
+
+tee:
 	mov rax, SYS_READ
 	mov rdi, STDIN
 	mov rsi, buffer
@@ -83,20 +101,90 @@ loop:
 
 	xchg rax, rdx ; size read into buffer
 
+	mov rcx, r14
+	sub rcx, handlers ; count of file handlers
+	shr rcx, 3
+
+write_loop:
+	push rcx
+
 	mov rax, SYS_WRITE
-	mov rdi, STDOUT
+	mov rdi, qword [handlers + (rcx - 1) * 8]
 	syscall
 
-	test r15, r15
-	jz loop
+	inc r14
+	pop rcx
+	loopnz write_loop
 
-	mov rax, SYS_WRITE
-	mov rdi, r15
-	syscall
-
-	jmp loop
+	jmp tee
 
 exit:
 	mov rax, SYS_EXIT
 	xor rdi, rdi
 	syscall
+
+; in
+;	rsi — *from
+; out
+;	rdx — length
+copy2buf:
+	xor rdx, rdx
+	mov rdi, buffer
+
+copy2buf_loop:
+	cmp byte [rsi], 0
+	movsb
+	je copy2buf_finish
+
+	inc rdx
+
+	cmp rdx, CHUNK
+	jl copy2buf_loop
+
+	mov byte [rdi], 0
+
+copy2buf_finish:
+	ret
+
+; in
+; 	r15 - append flag
+; out
+;	rax - file handler or 0 if error
+open_file:
+	test r15, r15
+	jz create_file
+	mov rsi, O_WRONLY | O_CREAT | O_APPEND
+	jmp open_file_call
+
+create_file:
+	mov rsi, O_WRONLY | O_CREAT | O_TRUNC
+
+open_file_call:
+	mov rax, SYS_OPEN
+	mov rdi, buffer
+	mov rdx, 0o660
+	syscall
+
+	ret
+
+; in
+;   rdx - length of filename string
+file_error:
+	mov rax, SYS_WRITE
+ 	mov rdi, STDERR
+ 	mov rsi, buffer
+ 	syscall
+
+ 	mov rax, SYS_WRITE
+ 	mov rsi, errorStr
+ 	mov rdx, errorStr.len
+ 	syscall
+ 	ret
+
+; in
+;	rax - file handler
+;	r14 — pointer to table of file handlers
+store_filehandler:
+	mov qword [r14], rax
+	add r14, 8
+	ret
